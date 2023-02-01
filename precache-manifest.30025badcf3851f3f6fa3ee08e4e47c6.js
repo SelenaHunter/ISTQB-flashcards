166f8eca87df87ebff4c9b2fd277363e704076bf
Selena Hunter <selenahunter99@gmail.com>
2023-02-01 15:17:30 -0600

    Updates

---
+++ precache-manifest.30025badcf3851f3f6fa3ee08e4e47c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06eaf8c7d1a09567d5912f4d0ac93866",
+    "url": "/istqb-flashcards/index.html"
+  },
+  {
+    "revision": "a4f4262e6ec6930bd3f3",
+    "url": "/istqb-flashcards/static/css/main.20fb528f.chunk.css"
+  },
+  {
+    "revision": "339ed6a2b03f1bb80df0",
+    "url": "/istqb-flashcards/static/js/2.7abf2bdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/istqb-flashcards/static/js/2.7abf2bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f4262e6ec6930bd3f3",
+    "url": "/istqb-flashcards/static/js/main.8dcb88d3.chunk.js"
+  },
+  {
+    "revision": "37451b8552a895e2c215",
+    "url": "/istqb-flashcards/static/js/runtime-main.58985f05.js"
+  },
+  {
+    "revision": "ddd68c7f66d7b7ff9c30494345888853",
+    "url": "/istqb-flashcards/static/media/texture1.ddd68c7f.jpg"
+  }
+]);
